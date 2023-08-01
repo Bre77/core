9ecb84d87597f1e4ab89519c8964f67af1d1a3fd
Brett <brett.whynot@gmail.com>
2023-08-01 05:38:25 +0000

    working POC

--- homeassistant/components/edgehub/__init__.py
+++ homeassistant/components/edgehub/__init__.py
@@ -1,29 +1,44 @@
-"""Support to send data to a Splunk instance."""
+"""Support to send data to a EdgeHub."""
 from datetime import datetime
 import logging
 
 from google.protobuf.timestamp_pb2 import Timestamp
 import paho.mqtt.client as mqtt
-from sensors_pb2 import ExternalSensorAvailability, SensorChannel, SensorDatapoint
+from .sensors_pb2 import ExternalSensorAvailability, SensorChannel, SensorDatapoint
 
 from homeassistant.const import (
+    CONF_HOST,
     EVENT_STATE_CHANGED,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv, state as state_helper
+from homeassistant.helpers.typing import ConfigType
+import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "edgehub"
-CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
+DEFAULT_HOST = "localhost"
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
 
-async def async_setup(hass: HomeAssistant) -> bool:
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Splunk component."""
 
+    host = config[DOMAIN].get(CONF_HOST, DEFAULT_HOST)
     client = mqtt.Client()
-    _LOGGER.info("Connecting to local MQTT")
-    client.connect("localhost", 1883, 60)
+    _LOGGER.info(f"Connecting to {host}:1883")
+    client.connect(host, 1883, 5)
+    _LOGGER.info(f"Connected to {host}:1883")
 
     entities = []
 
@@ -31,7 +46,11 @@ async def async_setup(hass: HomeAssistant) -> bool:
         """Listen for new messages on the bus and sends them to Splunk."""
 
         state = event.data.get("new_state")
+        eid = event.data.get("entity_id")
         if state is None:
+            return
+
+        if not state.attributes.get("state_class", False):
             return
 
         try:
@@ -40,32 +59,29 @@ async def async_setup(hass: HomeAssistant) -> bool:
             return
 
         # Send Available if required
-        if event.data.entity_id not in entities:
-            entities.append(event.data.entity_id)
+        if eid not in entities:
+            _LOGGER.info(f"Adding {eid} {state.name}")
+            entities.append(eid)
             client.publish(
                 "iotpuck/88e69537-27f0-4da1-89da-4e00fe90750c/sensors/availability",
                 payload=ExternalSensorAvailability(
-                    metrics=state["attributes"].get(
-                        "friendly_name", event.data.entity_id
-                    ),
-                    type=state["attributes"].get("device_class", "homeassistant"),
-                    id=event.data.entity_id,
+                    metrics=[eid],
+                    type=state.name,
+                    id=eid,
                     is_available=True,
                 ).SerializeToString(),
             )
 
         client.publish(
-            f"iotpuck/88e69537-27f0-4da1-89da-4e00fe90750c/sensors/{event.data.entity_id}/data",
+            f"iotpuck/88e69537-27f0-4da1-89da-4e00fe90750c/sensors/{eid}/data",
             payload=SensorDatapoint(
-                sensor_id=event.data.entity_id,
-                timestamp=Timestamp().FromDatetime(
-                    datetime.fromisoformat(state["last_changed"])
-                ),
+                sensor_id=eid,
+                timestamp=Timestamp().FromDatetime(state.last_changed),
                 value=value,
                 channel=SensorChannel(
-                    name=state["attributes"].get("friendly_name", event.data.entity_id),
-                    unit=state["attributes"].get("unit_of_measurement", ""),
-                    type=state["attributes"].get("device_class", "homeassistant"),
+                    name=eid,
+                    unit=state.attributes.get("unit_of_measurement", ""),
+                    type=state.name,
                 ),
                 is_sensor_enabled=True,
                 is_anomaly_enabled=False,
