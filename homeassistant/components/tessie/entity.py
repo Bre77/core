c21da5c4bed77251878cf96c4e63c79c0d570d9a
Brett Adams <brett.whynot@gmail.com>
2023-12-05 03:56:40 +0000

    remove async from set function

--- homeassistant/components/tessie/entity.py
+++ homeassistant/components/tessie/entity.py
@@ -76,7 +76,7 @@ class TessieEntity(CoordinatorEntity[TessieDataUpdateCoordinator]):
                 )
             raise HomeAssistantError from e
 
-    async def set(self, *args):
+    def set(self, *args):
         """Set a value in coordinator data."""
         for key, value in args:
             self.coordinator.data[key] = value
